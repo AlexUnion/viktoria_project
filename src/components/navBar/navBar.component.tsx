19f14c9c7a1e96266d0584cdf7e0d2aad53e7cf5
Al_chemick <alex2015mig@gmail.com>
2020-11-24 00:57:37 +0200

    'add_navbar'

---
+++ src/components/navBar/navBar.component.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './navBar.style.scss';
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { ReactComponent as FaceBook } from "../../assets/facebook.svg";
+import { ReactComponent as Instagram } from "../../assets/instagram.svg";
+
+function NavBar(): JSX.Element {
+    return (
+        <div className='nav_container'>
+            <Logo className='logo'/>
+            <div className='navigation'>
+                <div className='navigation_item'>
+                    Хто ми
+                </div>
+                <div className='navigation_item'>
+                    Що випікаємо
+                </div>
+                <div className='navigation_item'>
+                    Чому ми
+                </div>
+                <div className='navigation_item'>
+                    Контакти
+                </div>
+            </div>
+            <div className='contacts'>
+                <FaceBook className='contact_item'/>
+                <Instagram className='contact_item'/>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
